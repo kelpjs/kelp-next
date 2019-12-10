025a7eb020877852fd6a0c567587761071debcae
Lsong <song940@gmail.com>
2019-12-10 13:54:51 +0800

    add kelp-config

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 const path = require('path');
-
-const { 
-  CONFIG_DIR = 'config',
-  NODE_ENV = 'development'
-} = process.env;
+const config = require('kelp-config/config');
 
 const root = process.cwd();
 
@@ -12,6 +8,7 @@ const defaults = {
   path: {
     root,
     public: path.join(root, 'public'),
+    client: path.join(root, 'client'),
     server: path.join(root, 'server'),
     models: path.join(root, 'server/models'),
     plugins: path.join(root, 'server/plugins'),
@@ -23,15 +20,4 @@ const defaults = {
   plugins: [],
 };
 
-module.exports =  [
-  'default',
-  NODE_ENV,
-  'local'
-].reduce((config, conf) => {
-  let filename;
-  try{
-    filename = require.resolve(path.join(root, CONFIG_DIR, conf));
-  }catch(e){}
-  filename && Object.assign(config, require(filename));
-  return config;
-}, defaults);
+module.exports = config(defaults);
